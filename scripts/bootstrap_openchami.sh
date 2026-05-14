9de9f7440fef0f639b268570cf0f247a8f674669
Travis Cotton <trcotton@lanl.gov>
2026-05-14 07:39:48 -0600

    remove hydra stuff from bootstrap+openchami.sh
    Signed-off-by: Travis Cotton <trcotton@lanl.gov>

--- scripts/bootstrap_openchami.sh
+++ scripts/bootstrap_openchami.sh
@@ -62,20 +62,9 @@ create_secret_if_not_exists "bss_postgres_password" "$bss_postgres_password"
 smd_postgres_password=$(generate_random_password)
 create_secret_if_not_exists "smd_postgres_password" "$smd_postgres_password"
 
-# Hydra Postgres Password
-hydra_postgres_password=$(generate_random_password)
-create_secret_if_not_exists "hydra_postgres_password" "$hydra_postgres_password"
-
-# Hydra System Secret
-hydra_system_secret=$(generate_random_password)
-create_secret_if_not_exists "hydra_system_secret" "$hydra_system_secret"
-
-# HYDRA_DSN
-HYDRA_DSN="postgres://hydra-user:$(podman secret inspect hydra_postgres_password --showsecret | jq -r '.[0].SecretData')@postgres:5432/hydradb?sslmode=disable&max_conns=20&max_idle_conns=4"
-create_secret_if_not_exists "hydra_dsn" "$HYDRA_DSN"
 
 # POSTGRES_MULTIPLE_DATABASES
-POSTGRES_MULTIPLE_DATABASES="hmsds:smd-user:$(podman secret inspect smd_postgres_password --showsecret | jq -r '.[0].SecretData'),bssdb:bss-user:$(podman secret inspect bss_postgres_password --showsecret | jq -r '.[0].SecretData'),hydradb:hydra-user:$(podman secret inspect hydra_postgres_password --showsecret | jq -r '.[0].SecretData')"
+POSTGRES_MULTIPLE_DATABASES="hmsds:smd-user:$(podman secret inspect smd_postgres_password --showsecret | jq -r '.[0].SecretData'),bssdb:bss-user:$(podman secret inspect bss_postgres_password --showsecret | jq -r '.[0].SecretData')"
 create_secret_if_not_exists "postgres_multiple_databases" "$POSTGRES_MULTIPLE_DATABASES"
 
 # openchami.env Configuration
